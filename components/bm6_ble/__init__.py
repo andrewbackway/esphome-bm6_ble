3f5199841e6c6fe49bb44e1a9f8e6093a98c3a41
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2026-02-28 12:09:15 +1100

    #

--- components/bm6_ble/__init__.py
+++ components/bm6_ble/__init__.py
@@ -14,7 +14,7 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(ble_client.BLE_CLIENT_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # Use new_variable instead of new_PVar
-    var = cg.new_variable(config[CONF_ID])
+    # Added the second argument (rhs) to initialize the variable
+    var = cg.new_variable(config[CONF_ID], BM6Hub.new())
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
